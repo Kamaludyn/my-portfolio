88d3e9a78678c5b2410a9ad4a32dbb278515a568
kamaludyn <kamaluddeen305@gmail.com>
2025-05-10 04:06:30 -0800

    chore: move Google Analytics script to index.html head tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,17 @@ import Stack from "./components/Stack";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
-import GoogleAnalytics from "./components/GoogleAnalytics";
 
 function App() {
   return (
-    <>
-      <GoogleAnalytics trackingId="G-69H7BVG3HY" />
-      <main className="flex flex-col mx-auto w-[95%] md:w-[80%] text-gray-800 dark:text-gray-200 transition-colors mt-20">
-        <Nav />
-        <Hero />
-        <About />
-        <Stack />
-        <Projects />
-        <ContactMe />
-      </main>
-    </>
+    <main className="flex flex-col mx-auto w-[95%] md:w-[80%] text-gray-800 dark:text-gray-200 transition-colors mt-20">
+      <Nav />
+      <Hero />
+      <About />
+      <Stack />
+      <Projects />
+      <ContactMe />
+    </main>
   );
 }
 
